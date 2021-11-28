5e30ddd46bc1e2fa7db463d778868babb2690cd8
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-11-29 01:39:07 +0400

    XML Version Added

--- restapi/restapiAPP/views.py
+++ restapi/restapiAPP/views.py
@@ -6,8 +6,16 @@ from rest_framework.parsers import JSONParser
 from .models import Person
 from .serializers import PersonSerializer
 
+from rest_framework.decorators import api_view, parser_classes, renderer_classes
+from rest_framework.response import Response
+from rest_framework_xml.parsers import XMLParser
+from rest_framework_xml.renderers import XMLRenderer
+
+# @api_view(['GET'])
+# @renderer_classes((XMLRenderer,))
 @csrf_exempt
 def get_all_persons(request):
+    #JSON Version
 
     if request.method == 'GET':
         persons = Person.objects.all()
@@ -15,6 +23,13 @@ def get_all_persons(request):
         return JsonResponse(serializer.data, safe=False)
     else:
         return JsonResponse({'ErrorMessage: ' : 'Bad Request'}, status=400)
+
+    #XML Version
+
+    # if request.method == 'GET':
+    #     persons = Person.objects.all()
+    #     serializer = PersonSerializer(persons, many = True)
+    #     return Response(serializer.data)
 
 @csrf_exempt
 def get_person(request, pn):
@@ -30,8 +45,11 @@ def get_person(request, pn):
     else:
         return JsonResponse({'ErrorMessage: ' : 'Bad Request'}, status=400)
 
+# @api_view(['POST'])
+# @parser_classes((XMLParser,))
 @csrf_exempt
 def add_person(request):
+    #JSON Version
 
     if request.method == 'POST':
         data = JSONParser().parse(request)
@@ -43,6 +61,17 @@ def add_person(request):
         return JsonResponse(serializer.errors, status=400)
     else:
         return JsonResponse({'ErrorMessage: ' : 'Bad Request'}, status=400)
+
+    #XML Version
+
+    # if request.method == 'POST':
+    #     data = XMLParser().parse(request)
+    #     serializer = PersonSerializer(data=data)
+
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=201)
+    #     return Response(serializer.errors, status=400)
 
 @csrf_exempt
 def edit_person(request, pn):
